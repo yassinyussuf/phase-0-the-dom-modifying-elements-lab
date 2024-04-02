aa82365485e33304f3f02e36ad9c737d44485767
yassinyussuf <yassinyussuf414@gmail.com>
2024-04-02 10:05:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const title =document.querySelector("main#main")
+title.remove();
+const newHeader =document.createElement("h1");
+newHeader.setAttribute("id" ,"victory");
+newHeader.innerHtml ="YASSIN-YUSSUF , is the champion";
